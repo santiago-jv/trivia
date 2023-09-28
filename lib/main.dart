985e80eff97b41372f684b14660c22361a56c8fd
Alan Pino <alanpinodev@gmail.com>
2023-09-28 17:20:17 -0500

    fix: clean the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-//import 'package:trivia/presentation/screens/main_screen.dart';
 import 'package:trivia/presentation/screens/trivia_config_screen.dart';
 
 void main() {
   runApp(myApp());
-
 }
 
 class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Trivia_config_screen(),
+      home: TriviaConfigScreen(),
     );
-  }
-  
+  } 
 }
-
--- lib/presentation/screens/trivia_config_screen.dart
+++ lib/presentation/screens/trivia_config_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:trivia/presentation/widgets/dropdown_menu.dart';
 
-
-class Trivia_config_screen extends StatefulWidget{
-
-  Trivia_config_screen({super.key});
+class TriviaConfigScreen extends StatefulWidget {
+  TriviaConfigScreen({super.key});
 
   @override
-  State<Trivia_config_screen> createState() => _Trivia_config_screenState();
+  State<TriviaConfigScreen> createState() => _TriviaConfigScreenState();
 }
 
-class _Trivia_config_screenState extends State<Trivia_config_screen> {
+class _TriviaConfigScreenState extends State<TriviaConfigScreen> {
   late List<String> categories = [""];
   late List<String> difficult;
   late List<String> types;
@@ -18,28 +16,26 @@ class _Trivia_config_screenState extends State<Trivia_config_screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Trivia"),),
+      appBar: AppBar(
+        title: const Text("Trivia"),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-            const Text("Number of questions:"),
-            Container(width: 50,
-              child: TextField()),
-      
-            Text("Difficult"),  
-            CustomDropdownMenu(list: categories),
-      
-            Text("Category:"),
-            CustomDropdownMenu(list: categories),
-      
-            const Text("type of Trivia:"),
-            CustomDropdownMenu(list: categories),
-      
-            ElevatedButton(onPressed: (){}, child: Text("Start Trivia"))
-          ],),
+              const Text("Number of questions:"),
+              const SizedBox(width: 50, child: TextField()),
+              const Text("Difficult"),
+              CustomDropdownMenu(list: categories),
+              const Text("Category:"),
+              CustomDropdownMenu(list: categories),
+              const Text("type of Trivia:"),
+              CustomDropdownMenu(list: categories),
+              ElevatedButton(onPressed: () {}, child: const Text("Start Trivia"))
+            ],
+          ),
         ),
       ),
     );
